aafc34e617fd2baed2210100b6315fa4a217be60
Ovie Okeh <kevinokeh@gmail.com>
2020-05-18 00:05:29 +0100

    add map to page

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import IntroSection from './components/intro/Intro'
+import ContactSection from './components/contact-section/ContactSection'
+import MapSection from './components/map/Map' // import the map here
+import DisclaimerSection from './components/disclaimer/Disclaimer'
+import FooterSection from './components/footer/Footer'
+
+import './app.css'
+
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+} // our location object from earlier
+
+const App = () => (
+  <div className="App">
+    <IntroSection />
+    <ContactSection />
+    <MapSection location={location} zoomLevel={17} /> {/* include it here */}
+    <DisclaimerSection />
+    <FooterSection />
+  </div>
+)
+
+export default App
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/map-marker'
+
+import './map.css'
+
+const LocationPin = ({ text }) => (
+  <div className="pin">
+    <Icon icon={locationIcon} className="pin-icon" />
+    <p className="pin-text">{text}</p>
+  </div>
+)
+
+const Map = ({ location, zoomLevel }) => (
+  <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
+      </GoogleMapReact>
+    </div>
+  </div>
+)
+
+export default Map
